57cd052d76d8f06934e0b65c09547f4bc88d0c5b
Mikhail Abramov <beavisabra@yandex.ru>
2020-11-30 18:05:46 +0400

    Диалог открытия файла

--- main.cpp
+++ main.cpp
@@ -344,6 +344,37 @@ int main()
                  vybrannaya_category=button[i].category;
             }
 
+        //Диалог сохранения файла
+        if (clickButton(button[8].x, button[8].y))
+        {
+            OPENFILENAME ofn;       // common dialog box structure
+            char szFile[260];       // buffer for file name
+
+            // Initialize OPENFILENAME
+            ZeroMemory(&ofn, sizeof(ofn));
+            ofn.lStructSize = sizeof(ofn);
+            ofn.hwndOwner = txWindow();
+            ofn.lpstrFile = szFile;
+            // Set lpstrFile[0] to '\0' so that GetOpenFileName does not
+            // use the contents of szFile to initialize itself.
+            ofn.lpstrFile[0] = '\0';
+            ofn.nMaxFile = sizeof(szFile);
+            ofn.lpstrFilter = "Excel files 2003(*.xls)\0*.xls\0Excel files 2007(*.xlsx)\0*.xlsx\0";
+            ofn.nFilterIndex = 1;
+            ofn.lpstrFileTitle = NULL;
+            ofn.nMaxFileTitle = 0;
+            ofn.lpstrInitialDir = NULL;
+            ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+            // Display the Open dialog box.
+
+            if (GetOpenFileName(&ofn)==TRUE)
+            {
+
+
+            }
+        }
+
 
 
         for (int i = 0; i < N_variants; i++)
